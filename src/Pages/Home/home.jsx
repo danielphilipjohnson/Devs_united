afae25cbe83348d04c4be4b4a884af6c46595df5
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-20 22:01:10 +0000

    added twitter list to home

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../Context/UserContext";
 import { ColorContext } from "../../Context/ColorContext";
+import TweetsList from "../../Components/List/TweetsList";
 import LogoDevs from "../../assets/logo/logo.svg";
 import userPic from "../../assets/icons/user.svg";
 
@@ -18,12 +19,14 @@ function Home() {
                 <img className={`header_user ${colors}`} src={img} alt="user" /> : <img className={`header_user ${colors}`} src={userPic} alt="general-user" />
                 }
             </div> */}
-        <span className={`color ${colors}`}>USERNAME</span>
         <img className="header_logo" src={LogoDevs} alt="Devs_United Logo" />
         <h2 className="header_text">
           Devs_<span>United</span>
         </h2>
       </header>
+      <TweetsList/>
+
+
     </div>
   );
 }
