6bf7ba316f0c5b16d45be22fcd1e22dd9efe899d
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-20 21:43:19 +0000

    Hide broken image

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,28 +1,31 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../../Context/UserContext'
-import { ColorContext } from '../../Context/ColorContext'
-import LogoDevs from '../../assets/logo/logo.svg'
-import userPic from '../../assets/icons/user.svg'
+import React, { useContext } from "react";
+import { UserContext } from "../../Context/UserContext";
+import { ColorContext } from "../../Context/ColorContext";
+import LogoDevs from "../../assets/logo/logo.svg";
+import userPic from "../../assets/icons/user.svg";
 
-function Home({tweetData}) {
 
-    const { colors } = useContext(ColorContext);
-    const { user } = useContext(UserContext);
-    const { img } = tweetData;
+function Home() {
+  const { colors } = useContext(ColorContext);
+  const { user } = useContext(UserContext);
+  // const { img } = tweetData;
 
   return (
     <div>
-        <header className="header">
-            <div>
+      <header className="header">
+        {/* <div>
                 {user ? 
                 <img className={`header_user ${colors}`} src={img} alt="user" /> : <img className={`header_user ${colors}`} src={userPic} alt="general-user" />
                 }
-            </div>
-            <img className="header_logo" src={LogoDevs} alt="Devs_United Logo" />
-            <h2 className="header_text">Devs_<span>United</span></h2>
-        </header>
+            </div> */}
+        <span className={`color ${colors}`}>USERNAME</span>
+        <img className="header_logo" src={LogoDevs} alt="Devs_United Logo" />
+        <h2 className="header_text">
+          Devs_<span>United</span>
+        </h2>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
