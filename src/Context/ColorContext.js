6eb908b41dd01d5b6086af8e247393d459f7aa21
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-20 21:17:42 +0000

    Cambiar default color

--- src/Context/ColorContext.js
+++ src/Context/ColorContext.js
@@ -1,16 +1,16 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
 export const ColorContext = createContext();
 
-const ColorProvider = ({children}) => {
+const ColorProvider = ({ children }) => {
+    
+  const [colors, setColors] = useState("green");
 
-    const [colors, setColors] = useState('#FF865C');
-
-    return (
-        <ColorContext.Provider value={{colors, setColors}}>
-        {children}
-        </ColorContext.Provider>
-    )
-}
+  return (
+    <ColorContext.Provider value={{ colors, setColors }}>
+      {children}
+    </ColorContext.Provider>
+  );
+};
 
 export default ColorProvider;
